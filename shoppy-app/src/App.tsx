a9c97587274737ff7b6b6c58c6c2e2c4d4af11a8
Fred Zhao <yunzez@uw.edu>
2022-10-26 23:53:51 -0400

    setup

---
+++ shoppy-app/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import {Routes, Route} from "react-router-dom"
+import {Container} from 'react-bootstrap'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Container>
+      <h1>hi</h1>
+    </Container>
+  )
+}
+
+export default App
