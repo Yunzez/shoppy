f28c33d2556562ab5512fc1d17346760948eeb51
Fred Zhao <yunzez@uw.edu>
2022-10-28 14:12:05 -0400

    add intrp

--- shoppy-app/src/pages/Home.tsx
+++ shoppy-app/src/pages/Home.tsx
@@ -1,3 +1,9 @@
 export function Home() {
-    return (<h2>home</h2>)
+    return (
+        <div className="mt-4">
+            <h2>
+                This is a practice website for React + Express + Graphql in TypeScript, hosted on firebase(NoSQL database)
+            </h2>
+            <a href="https://github.com/Yunzez/shoppy"> Check out the repo</a>
+        </div>)
 }
